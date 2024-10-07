41aba88ee7096c37e8310ef3038025ddf9a74ff1
dhillonJas <jasdeep.dhillon10@gmail.com>
2024-10-07 15:23:49 -0500

    WIP: Add team filters

--- src/Components/Team.js
+++ src/Components/Team.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState , useEffect, useCallback} from 'react';
 import DropdownComp from './Dropdown';
 import team_data from '../data/tables/team_table.json'
 import { Maps, Events } from '../data/dropdownoptions';
@@ -7,10 +7,12 @@ import { INFORMATION, ADVANCED, teams_columns } from '../data/columns_names';
 
 function Team({ onFilter, onViewModeChange}) {
 
+  const ALL = 'All'
   const [isAdvanced, setIsAdvanced] = useState(false)
-  const [event, setEvent] = useState('Any')
-  const [mapName, setMapName] = useState('Any')
-  // const [opponet, setOpponent] = useState('Any')
+  const [event, setEvent] = useState(ALL)
+  const [mapName, setMapName] = useState(ALL)
+  const [opponent] = useState(ALL)
+  const [dataToShow, setDataToShow] = useState(team_data)
 
   
   // function getData() {
@@ -18,47 +20,70 @@ function Team({ onFilter, onViewModeChange}) {
   //   return [{"Name" : "Sentinels"}]
   // }
 
+
   const handleFilter = useCallback(() => {
-    if (event === 'All')
+    if (event === ALL && mapName === ALL && opponent === ALL)
         onFilter(team_data)
     else{
-        const teamTotals = {};
-
+        const filtered_teams_data = {};
+        let dataToSum = []
         team_data.forEach(item => {
-          if (!teamTotals[item.id]) {
-            teamTotals[item.id] = { 
-                                      name: item.name,
-                                      region: item.region,
-                                      events_attended: item.events_attended,
-                                      events_won: item.events_won,
-                                      events_best_placements: item.events_best_placements,
-                                      maps_won: 0,
-                                      maps_lost: 0,
-                                      map_picks_won: 0,
-                                      map_picks_lost: 0,
-                                      rounds_won: 0,
-                                      rounds_lost: 0,
-                                      maps:[]
-                                    };
+        //   if (!filtered_teams_data[item.id]) {
+        //     filtered_teams_data[item.id] = { 
+        //                               name: item.name,
+        //                               region: item.region,
+        //                               events_attended: item.events_attended,
+        //                               events_won: item.events_won,
+        //                               events_best_placements: item.events_best_placements,
+        //                               maps_won: 0,
+        //                               maps_lost: 0,
+        //                               map_picks_won: 0,
+        //                               map_picks_lost: 0,
+        //                               rounds_won: 0,
+        //                               rounds_lost: 0,
+        //                               maps:[]
+        //                             };
+        //   }
+          let event_obj = Object.values(item['event_stats'])
+          if (event !== ALL)
+          {
+            if (item['event_stats'][Events[event]])
+              event_obj = item['event_stats'][Events[event]]
           }
-        }
+          
+          let map_obj = []
+          let map_data = []
+          if (mapName === ALL)
+            {
+              for (let key in event_obj['event_maps']){
+                map_obj.push(event_obj['event_maps'][key])
+              }
+            }
+          else if (mapName !== ALL){
+            if (event_obj['event_maps'][mapName])
+              map_obj = event_obj['event_maps'][mapName]
+          }
 
-      
-
-        )
+          if (map_obj)
+            dataToSum.push(map_obj)
+          
+        },
+        console.log(dataToSum)
+        
+      )
     }
-    }, [onFilter])
+    }, [onFilter, event, mapName, opponent])
 
 useEffect(() => {
   handleFilter()
-}, [event]);
+}, [handleFilter, event, mapName, opponent]);
 
 
   useEffect(() => {
     let viewMode = isAdvanced  ?  ADVANCED: INFORMATION
     onViewModeChange(teams_columns[viewMode]); // Pass columns to Home.js
-    onFilter(team_data)
-  }, [onViewModeChange, isAdvanced, onFilter]);
+    onFilter(dataToShow)
+  }, [onViewModeChange, isAdvanced, onFilter, dataToShow]);
 
   const handleClick = () => {
     setIsAdvanced(!isAdvanced);
@@ -71,7 +96,7 @@ useEffect(() => {
        </Button>
         <DropdownComp   selectedValue={event}
                         setSelectedValue={setEvent}
-                        options={Events}> 
+                        options={Object.keys(Events)}> 
         </DropdownComp>
         <DropdownComp   selectedValue={mapName}
                         setSelectedValue={setMapName}
--- src/data/dropdownoptions.js
+++ src/data/dropdownoptions.js
@@ -14,18 +14,18 @@ export const Maps = [
   ];
 
   // should match the event_name in event_table.json
-export const Events = [
-    'All',
-    'Valorant Champions 2024',
-    'Masters Shanghai 2024',
-    'Masters Madrid 2024',
-    'Valorant Champions 2023',
-    'Masters Tokyo 2023',
-    'Masters S\u00e3o Paulo LOCK//IN 2023',
-    'Valorant Champions 2022',
-    'Masters Copenhagen 2022',
-    'Masters Reykjav\u00edk 2022',
-    'Valorant Champions 2021',
-    'Masters Berlin 2021',
-    'Masters Reykjav\u00edk 2021'
-  ]
+export const Events = {
+    'All':0,
+    'Valorant Champions 2024':2097,
+    'Masters Shanghai 2024':1999,
+    'Masters Madrid 2024':1921,
+    'Valorant Champions 2023':1657,
+    'Masters Tokyo 2023':1494,
+    'Masters S\u00e3o Paulo LOCK//IN 2023':1188,
+    'Valorant Champions 2022':1015,
+    'Masters Copenhagen 2022':1014,
+    'Masters Reykjav\u00edk 2022':926,
+    'Valorant Champions 2021':449,
+    'Masters Berlin 2021':466,
+    'Masters Reykjav\u00edk 2021':353
+}
