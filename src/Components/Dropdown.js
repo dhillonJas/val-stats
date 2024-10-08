0611f5820e7fdac10b97637999a4e8e1a631b792
Jasdeep Singh <55818488+dhillonJas@users.noreply.github.com>
2024-10-08 19:07:02 +0000

    Add search to dropdown

--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,23 +1,50 @@
+import React, { useState } from 'react';
 import Dropdown from 'react-bootstrap/Dropdown';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function DropdownComp({ selectedValue, setSelectedValue, options }) {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [isOpen, setIsOpen] = useState(false);
+
+  // Filter options based on the search term
+  const filteredOptions = options.filter(option =>
+      option.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   const handleSelect = (eventKey) => {
     setSelectedValue(eventKey); // Update the selected value in the parent component
   };
 
   return (
-    <Dropdown onSelect={handleSelect}>
+    <Dropdown onSelect={handleSelect}  show={isOpen} onToggle={() => setIsOpen(!isOpen)}>
 
       <Dropdown.Toggle variant="success" id="dropdown-split-basic" >
       {selectedValue}
       </Dropdown.Toggle>
 
       <Dropdown.Menu style={{ maxHeight: '200px', overflowY: 'auto' }}>
-        {options.map((option, index) => (
-          <Dropdown.Item key={index} eventKey={option}>{option}</Dropdown.Item>
-        ))}
+      <div style={{ padding: '10px' }}>
+                    <input
+                        type="text"
+                        placeholder="Search..."
+                        value={searchTerm}
+                        onChange={e => setSearchTerm(e.target.value)}
+                        style={{ width: '100%', marginBottom: '10px' }}
+                    />
+        </div>
+        {filteredOptions.length > 0 ? 
+                (
+                    filteredOptions.map((option, index) => (
+                        <Dropdown.Item key={index} eventKey={option} onClick={() => { setSearchTerm(''); setIsOpen(false); }}>
+                            {option}
+                        </Dropdown.Item>
+                    ))
+                ) : 
+                (
+                    <Dropdown.Item disabled>No options found</Dropdown.Item>
+                )
+        }
       </Dropdown.Menu>
     </Dropdown>
   );
