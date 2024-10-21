a7a67035340593b0a52d2d77217a20c3007e456b
Jasdeep Singh <55818488+dhillonJas@users.noreply.github.com>
2024-10-21 14:57:34 +0000

    Update logo path names

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,9 @@ import './css/Header.css'; // Importing the CSS file for styling
 const Header = () => {
   return (
     <header className="header">
-      <img src="/logo-vandal.png" alt="Logo" className="logo" />
+      <img src={`${process.env.PUBLIC_URL}/logo_vandal.png`} alt="Logo" className="logo" />
       <h1 className="title">Valorant Stats Central</h1>   
-      <img src="/logo-phantom.png" alt="Logo" className="logo" />
+      <img src={`${process.env.PUBLIC_URL}/logo_phantom.png`} alt="Logo" className="logo" />
     </header>
   );
 };
